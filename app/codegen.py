18e6103df7076e268444caf74adb56886228e007
ydcjeff <ydcjeff@outlook.com>
2021-03-15 15:45:23 +0630

    test: generate configs with hypothesis

--- app/codegen.py
+++ app/codegen.py
@@ -2,6 +2,7 @@
 """
 import shutil
 from pathlib import Path
+from typing import Optional
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -12,7 +13,7 @@ class CodeGenerator:
         self.template_list = [p.stem for p in Path(templates_dir).iterdir() if p.is_dir()]
         self.env = Environment(loader=FileSystemLoader(templates_dir), trim_blocks=True, lstrip_blocks=True)
 
-    def render_templates(self, template_name: str, config: dict):
+    def render_templates(self, template_name: str, config: dict, output_path_name: Optional[str] = None):
         """Renders all the templates from template folder for the given config."""
         file_template_list = (
             template for template in self.env.list_templates(".jinja") if template.startswith(template_name)
@@ -24,7 +25,10 @@ class CodeGenerator:
             code = template.render(**config)
             # Write python file
             fname = fname.replace(f"{template_name}/", "").replace(".jinja", "")
-            self.generate(template_name, fname, code)
+            if output_path_name:
+                self.generate(output_path_name, fname, code)
+            else:
+                self.generate(template_name, fname, code)
             yield fname, code
 
     def generate(self, template_name: str, fname: str, code: str) -> None:
