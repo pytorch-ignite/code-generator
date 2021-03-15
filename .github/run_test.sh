3b6ad67cccc0a335848b4f3603a86c63f6a7cab0
ydcjeff <ydcjeff@outlook.com>
2021-03-15 21:34:41 +0630

    typo -.-

--- .github/run_test.sh
+++ .github/run_test.sh
@@ -5,8 +5,8 @@ set -xeu
 if [ $1 = "generate" ]; then
     python ./tests/templates/gen_image_classification.py
 elif [ $1 = "unittest" ]; then
-    pytest ./tests/unittest -vvv -ra --color=yes
+    pytest ./tests/unittest -vvv -ra --color=yes --durations=0
 elif [ $1 = "integration" ]; then
-    cd ./tests/templates/dist/image_classfication
+    cd ./tests/templates/dist/image_classification
     python main.py --verbose
 fi
