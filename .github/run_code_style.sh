294fde24d5be090a99eafad28778051f7c6670fc
ydcjeff <ydcjeff@outlook.com>
2021-03-16 10:45:29 +0630

    rm ci and style configs

--- .github/run_code_style.sh
+++ .github/run_code_style.sh
@@ -3,12 +3,12 @@
 set -xeu
 
 if [ $1 = "lint" ]; then
-    flake8 app templates tests --config .flake8
-    isort app templates tests --check --settings pyproject.toml
-    black app templates tests --check --config pyproject.toml
+    flake8 app templates --max-line-length 120
+    isort app templates --check --settings pyproject.toml
+    black app templates --check --config pyproject.toml
 elif [ $1 = "fmt" ]; then
-    isort app templates tests --color --settings pyproject.toml
-    black app templates tests --config pyproject.toml
+    isort app templates --color --settings pyproject.toml
+    black app templates --config pyproject.toml
 elif [ $1 = "install" ]; then
     pip install flake8 "black==20.8b1" "isort==5.7.0"
 fi
