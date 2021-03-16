cc8900f533c747df5cd3e2466f4f5148faba4804
ydcjeff <ydcjeff@outlook.com>
2021-03-16 18:09:43 +0630

    epochs and epoch_length 2

--- tests/integration/test_image_classification.sh
+++ tests/integration/test_image_classification.sh
@@ -1,10 +1,10 @@
 cd ./tests/dist/image_classification
 python main.py \
     --verbose \
-    --train_max_epochs 1 \
-    --train_epoch_length 1 \
-    --eval_max_epochs 1 \
-    --eval_epoch_length 1 \
+    --train_max_epochs 2 \
+    --train_epoch_length 2 \
+    --eval_max_epochs 2 \
+    --eval_epoch_length 2 \
     --log_train 1 \
     --log_eval 1 \
     --train_batch_size 4 \
