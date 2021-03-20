94478151ff98f3faa36d4199f94f03ac966d5eac
ydcjeff <ydcjeff@outlook.com>
2021-03-20 22:30:40 +0630

    port dcgan from ignite

--- templates/gan/sidebar.py
+++ templates/gan/sidebar.py
@@ -1,12 +1,22 @@
-import sys
+from argparse import Namespace
 
 import streamlit as st
+import toml
 
-sys.path.append('./templates/')
 
-from base.sidebar import get_configs as base_configs
+params = toml.load("./templates/gan/config.toml")
 
 
 def get_configs() -> dict:
-    config = base_configs()
+    config = {}
+    with st.beta_expander("GAN Configurations"):
+        st.info("Those in the parenthesis are used in the code.")
+        for k, v in params.items():
+            for kv, vv in v.items():
+                if k in ("nproc_per_node"):  # , "nnodes", "node_rank", "master_addr", "master_port"):
+                    if st.checkbox("Use distributed training"):
+                        config[k] = getattr(st, kv)(**vv)
+                else:
+                    config[k] = getattr(st, kv)(**vv)
+
     return config
