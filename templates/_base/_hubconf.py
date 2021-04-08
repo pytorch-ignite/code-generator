fe9c7371b25e8913d9b7981ba37c471e8aafb5fd
ydcjeff <ydcjeff@outlook.com>
2021-04-08 19:52:27 +0630

    refactor(templates): remove hubconf.py

--- templates/_base/_hubconf.py
+++
@@ -1,6 +0,0 @@
-# Optional list of dependencies required by the package
-dependencies = ["torch"]
-
-# import your models to be used easily via `torch.hub.load()`.
-# PyTorch Hub docs : https://pytorch.org/docs/stable/hub.html
-# torchvision hubconf.py : https://github.com/pytorch/vision/blob/master/hubconf.py
