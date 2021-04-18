275b0a24ff90a8c905c29d38dad0a8c7fb103986
Taras Savchyn <trsvchn@gmail.com>
2021-04-18 21:38:51 +0300

    Adjust tests run for text_classifier

--- .github/run_test.sh
+++ .github/run_test.sh
@@ -20,29 +20,56 @@ elif [ $1 == "unittest" ]; then
 elif [ $1 == "default" ]; then
     for file in $(find ./tests/dist -iname "main.py" -not -path "./tests/dist/launch/*" -not -path "./tests/dist/spawn/*" -not -path "./tests/dist/single/*")
     do
-        python $file --verbose --log_every_iters 2 --num_workers 1 --epoch_length 10
+        if [ $file == "./tests/dist/text_classification/main.py" ]; then
+            python $file --verbose --log_every_iters 1 --num_workers 1 --epoch_length 5 --batch_size 2
+        else
+            python $file --verbose --log_every_iters 2 --num_workers 1 --epoch_length 10
+        fi
     done
 elif [ $1 == "launch" ]; then
     for file in $(find ./tests/dist/launch -iname "main.py" -not -path "./tests/dist/launch/single/*")
     do
-        python -m torch.distributed.launch \
-            --nproc_per_node 2 \
-            --use_env $file \
-            --verbose \
-            --backend gloo \
-            --num_workers 1 \
-            --epoch_length 10 \
-            --log_every_iters 2
+        if [ $file == "./tests/dist/launch/text_classification/main.py" ]; then
+            python -m torch.distributed.launch \
+                --nproc_per_node 2 \
+                --use_env $file \
+                --verbose \
+                --backend gloo \
+                --num_workers 1 \
+                --epoch_length 5 \
+                --log_every_iters 1
+                --batch_size 2
+        else
+            python -m torch.distributed.launch \
+                --nproc_per_node 2 \
+                --use_env $file \
+                --verbose \
+                --backend gloo \
+                --num_workers 1 \
+                --epoch_length 10 \
+                --log_every_iters 2
+        fi
     done
 elif [ $1 == "spawn" ]; then
     for file in $(find ./tests/dist/spawn -iname "main.py" -not -path "./tests/dist/spawn/single/*")
     do
-        python $file \
-            --verbose \
-            --backend gloo \
-            --num_workers 1 \
-            --epoch_length 10 \
-            --nproc_per_node 2 \
-            --log_every_iters 2
+        if [ $file == "./tests/dist/spawn/text_classification/main.py" ]; then
+            python $file \
+                --verbose \
+                --backend gloo \
+                --num_workers 1 \
+                --epoch_length 5 \
+                --nproc_per_node 2 \
+                --log_every_iters 1
+                --batch_size 2
+        else
+            python $file \
+                --verbose \
+                --backend gloo \
+                --num_workers 1 \
+                --epoch_length 10 \
+                --nproc_per_node 2 \
+                --log_every_iters 2
+        fi
     done
 fi
