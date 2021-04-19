df1794e292c00f878e73d11004f74fb75f0e9da2
Taras Savchyn <trsvchn@gmail.com>
2021-04-19 14:39:47 +0300

    Fix linting issues

--- templates/text_classification/_sidebar.py
+++ templates/text_classification/_sidebar.py
@@ -1,5 +1,4 @@
 import sys
-from argparse import Namespace
 
 import streamlit as st
 
@@ -22,11 +21,17 @@ def get_configs() -> dict:
     st.header("Transformer")
 
     st.subheader("Model Options")
-    config["model"] = st.selectbox("Model name (from transformers) to setup model, tokenize and config to train", options=["bert-base-uncased"])
+    config["model"] = st.selectbox(
+        "Model name (from transformers) to setup model, tokenize and config to train", options=["bert-base-uncased"]
+    )
     config["model_dir"] = st.text_input("Cache directory to download the pretrained model", value="./")
     config["tokenizer_dir"] = st.text_input("Tokenizer cache directory", value="./tokenizer")
-    config["num_classes"] = st.number_input("Number of target classes. Default, 1 (binary classification)", min_value=0, value=1)
-    config["max_length"] = st.number_input("Maximum number of tokens for the inputs to the transformer model", min_value=1, value=256)
+    config["num_classes"] = st.number_input(
+        "Number of target classes. Default, 1 (binary classification)", min_value=0, value=1
+    )
+    config["max_length"] = st.number_input(
+        "Maximum number of tokens for the inputs to the transformer model", min_value=1, value=256
+    )
     config["dropout"] = st.number_input("Dropout probability", min_value=0.0, max_value=1.0, value=0.3, format="%f")
     config["n_fc"] = st.number_input("Number of neurons in the last fully connected layer", min_value=1, value=768)
     st.markdown("---")
@@ -41,16 +46,26 @@ def get_configs() -> dict:
     st.markdown("---")
 
     st.subheader("Optimizer Options")
-    config["learning_rate"] = st.number_input("Peak of piecewise linear learning rate scheduler", min_value=0.0, value=5e-5, format="%e")
+    config["learning_rate"] = st.number_input(
+        "Peak of piecewise linear learning rate scheduler", min_value=0.0, value=5e-5, format="%e"
+    )
     config["weight_decay"] = st.number_input("Weight decay", min_value=0.0, value=0.01, format="%f")
     st.markdown("---")
 
     st.subheader("Training Options")
     config["max_epochs"] = st.number_input("Number of epochs to train the model", min_value=1, value=3)
-    config["num_warmup_epochs"] = st.number_input("Number of warm-up epochs before learning rate decay", min_value=0, value=0)
-    config["validate_every"] = st.number_input("Run model's validation every validate_every epochs", min_value=0, value=1)
-    config["checkpoint_every"] = st.number_input("Store training checkpoint every checkpoint_every iterations", min_value=0, value=1000)
-    config["log_every_iters"] = st.number_input("Argument to log batch loss every log_every_iters iterations. 0 to disable it", min_value=0, value=15)
+    config["num_warmup_epochs"] = st.number_input(
+        "Number of warm-up epochs before learning rate decay", min_value=0, value=0
+    )
+    config["validate_every"] = st.number_input(
+        "Run model's validation every validate_every epochs", min_value=0, value=1
+    )
+    config["checkpoint_every"] = st.number_input(
+        "Store training checkpoint every checkpoint_every iterations", min_value=0, value=1000
+    )
+    config["log_every_iters"] = st.number_input(
+        "Argument to log batch loss every log_every_iters iterations. 0 to disable it", min_value=0, value=15
+    )
     st.markdown("---")
 
     distributed_options(config)
