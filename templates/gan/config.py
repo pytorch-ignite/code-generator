3771d6517b6bba0762384487de1ee4528a115930
Taras Savchyn <trsvchn@gmail.com>
2021-04-19 15:42:15 +0300

    Periods

--- templates/gan/config.py
+++ templates/gan/config.py
@@ -6,56 +6,56 @@ UPDATES = {
         "default": "{{ dataset }}",
         "type": str,
         "choices": ["cifar10", "lsun", "imagenet", "folder", "lfw", "fake", "mnist"],
-        "help": "dataset to use, Default: %(default)s",
+        "help": "dataset to use. Default: %(default)s",
     },
     "data_path": {
         "default": "{{ data_path }}",
         "type": str,
-        "help": "datasets path, Default: %(default)s",
+        "help": "datasets path. Default: %(default)s",
     },
     # dataloader options
     "batch_size": {
         "default": {{batch_size}},
         "type": int,
-        "help": "will be equally divided by number of GPUs if in distributed, Default: %(default)s",
+        "help": "will be equally divided by number of GPUs if in distributed. Default: %(default)s",
     },
     "num_workers": {
         "default": {{num_workers}},
         "type": int,
-        "help": "num_workers for DataLoader, Default: %(default)s",
+        "help": "num_workers for DataLoader. Default: %(default)s",
     },
     # optimizer options
     "beta_1": {
         "default": {{beta_1}},
         "type": float,
-        "help": "beta_1 for Adam optimizer, Default: %(default)s",
+        "help": "beta_1 for Adam optimizer. Default: %(default)s",
     },
     "lr": {
         "default": {{lr}},
         "type": float,
-        "help": "learning rate used by torch.optim.*, Default: %(default)s",
+        "help": "learning rate used by torch.optim.*. Default: %(default)s",
     },
     # training options
     "max_epochs": {
         "default": {{max_epochs}},
         "type": int,
-        "help": "max_epochs of ignite.Engine.run() for training, Default: %(default)s",
+        "help": "max_epochs of ignite.Engine.run() for training. Default: %(default)s",
     },
     # model options
     "z_dim": {
         "default": {{z_dim}},
         "type": int,
-        "help": "size of the latent z vector, Default: %(default)s",
+        "help": "size of the latent z vector. Default: %(default)s",
     },
     "g_filters": {
         "default": {{g_filters}},
         "type": int,
-        "help": "number of filters in the second-to-last generator deconv layer, Default: %(default)s",
+        "help": "number of filters in the second-to-last generator deconv layer. Default: %(default)s",
     },
     "d_filters": {
         "default": {{d_filters}},
         "type": int,
-        "help": "number of filters in first discriminator conv layer, Default: %(default)s",
+        "help": "number of filters in first discriminator conv layer. Default: %(default)s",
     },
 }
 
--- templates/image_classification/config.py
+++ templates/image_classification/config.py
@@ -5,56 +5,56 @@ UPDATES = {
     "data_path": {
         "default": "{{ data_path }}",
         "type": str,
-        "help": "datasets path, Default: %(default)s",
+        "help": "datasets path. Default: %(default)s",
     },
     # dataloader options
     "train_batch_size": {
         "default": {{train_batch_size}},
         "type": int,
-        "help": "will be equally divided by number of GPUs if in distributed, Default: %(default)s",
+        "help": "will be equally divided by number of GPUs if in distributed. Default: %(default)s",
     },
     "eval_batch_size": {
         "default": {{eval_batch_size}},
         "type": int,
-        "help": "will be equally divided by number of GPUs if in distributed, Default: %(default)s",
+        "help": "will be equally divided by number of GPUs if in distributed. Default: %(default)s",
     },
     "num_workers": {
         "default": {{num_workers}},
         "type": int,
-        "help": "num_workers for DataLoader, Default: %(default)s",
+        "help": "num_workers for DataLoader. Default: %(default)s",
     },
     # optimizer options
     "lr": {
         "default": {{lr}},
         "type": float,
-        "help": "learning rate used by torch.optim.*, Default: %(default)s",
+        "help": "learning rate used by torch.optim.*. Default: %(default)s",
     },
     "momentum": {
         "default": {{momentum}},
         "type": float,
-        "help": "momentum used by torch.optim.SGD, Default: %(default)s",
+        "help": "momentum used by torch.optim.SGD. Default: %(default)s",
     },
     "weight_decay": {
         "default": {{weight_decay}},
         "type": float,
-        "help": "weight_decay used by torch.optim.SGD, Default: %(default)s",
+        "help": "weight_decay used by torch.optim.SGD. Default: %(default)s",
     },
     # training options
     "max_epochs": {
         "default": {{max_epochs}},
         "type": int,
-        "help": "max_epochs of ignite.Engine.run() for training, Default: %(default)s",
+        "help": "max_epochs of ignite.Engine.run() for training. Default: %(default)s",
     },
     "num_warmup_epochs": {
         "default": {{num_warmup_epochs}},
         "type": int,
-        "help": "number of warm-up epochs before learning rate decay, Default: %(default)s",
+        "help": "number of warm-up epochs before learning rate decay. Default: %(default)s",
     },
     # model options
     "model": {
         "default": "{{model}}",
         "type": str,
-        "help": "model to use, available all torchvision classification models, Default: %(default)s",
+        "help": "model to use, available all torchvision classification models. Default: %(default)s",
     }
 }
 
