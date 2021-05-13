8a8c364fafdf20ee8504f004b9db54d9bc82eac4
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-13 18:17:57 +0630

    wip: restructure and add JSDoc in store.js

--- src/store.js
+++ src/store.js
@@ -27,36 +27,63 @@ ejs.delimiter = ':::'
 ejs.openDelimiter = '#'
 ejs.closeDelimiter = '#'
 
-export let files = {}
+// to store all fetch template files
+let files = {}
+
+// main reactive object
+// store.code - the final rendered code to be included in archive
+// store.config - the internal config to track user input
 export const store = reactive({
   code: {},
-  config: {}
+  config: {
+    template: ''
+  }
 })
 
+/**
+ * @param {string} key
+ * @param {string | number | boolean | null} value
+ */
+// save config if the value changes or
+// if the key is not saved before
 export function saveConfig(key, value) {
   if (store.config[key] === undefined || store.config[key] !== value) {
     store.config[key] = value
   }
 }
 
+// render the code
 export async function genCode() {
   console.log('watch effect')
-  if (files && Object.keys(files).length) {
-    for (const file in files) {
-      // console.log(file)
+  const currentFiles = files[store.config.template]
+  if (currentFiles && Object.keys(currentFiles).length) {
+    for (const file in currentFiles) {
       store.code[file] = ejs.render(files[file], store.config)
     }
   }
 }
 
+/**
+ * @param {string} template
+ */
+// fetch the templates
+// save them in the files[template]
 export async function fetchTemplates(template) {
-  for (const filename in templates[template]) {
-    const res = await fetch(`${url}/${template}/${filename}`)
-    const text = await res.text()
-    files[filename] = text
+  // fetch the template if there is no fetch of template before
+  if (files[template] === undefined) {
+    for (const filename in templates[template]) {
+      const response = await fetch(`${url}/${template}/${filename}`)
+      files[template][filename] = await response.text()
+    }
+
+    // calling genCode explicitly here
+    // since fetch templates is running asynchronously
+    // so that rendered code will show up
+    // after choosing the template
+    genCode()
   }
-  console.log('after fetch')
-  genCode()
 }
 
+// watch the store.config
+// if that changed, call the genCode function
 watch(store.config, () => genCode())
