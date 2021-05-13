74210c7c321462bc155cc52b7c4b1b8e79ebfeb3
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-14 01:29:58 +0630

    chore: add color

--- scripts/check_copies.py
+++ scripts/check_copies.py
@@ -2,19 +2,24 @@
 
 from pathlib import Path
 
+
 def check_utils():
-  with open('./src/templates/template-common/utils.py', 'r') as f:
-    common_utils = f.read()
+    red = "\033[31m"
+    green = "\033[32m"
+    reset = "\033[0m"
 
-  path = Path('./src/templates/')
+    with open("./src/templates/template-common/utils.py", "r") as f:
+        common_utils = f.read()
 
-  for file in path.rglob('**/utils.py'):
-    utils = file.read_text('utf-8')
-    if (utils.find(common_utils) > -1):
-      print('Matched', file)
-    else:
-      print('Unmatched', file)
+    path = Path("./src/templates/")
+
+    for file in path.rglob("**/utils.py"):
+        utils = file.read_text("utf-8")
+        if utils.find(common_utils) > -1:
+            print(green, "Matched", file, reset)
+        else:
+            print(red, "Unmatched", file, reset)
 
 
-if __name__ == '__main__':
-  check_utils()
+if __name__ == "__main__":
+    check_utils()
