f0d08e81ecacaf18404a212fc12f0be4a5edb231
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-05-14 11:24:10 +0630

    refactor: extract footer into Footer component (#115)

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer-and-badge">
+    <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
+      <img src="../assets/netlify-color-accent.svg" alt="Deploys by Netlify" />
+    </a>
+    <p>
+      BSD-3-Clause Licensed | Copyright &copy; 2021-present PyTorch-Ignite Team
+      & Code-Generator Contributors
+    </p>
+  </footer>
+</template>
+
+<style>
+.footer-and-badge {
+  border-top: 1px solid var(--c-white-dark);
+  text-align: center;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  padding: 3rem 1.5rem;
+}
+</style>
