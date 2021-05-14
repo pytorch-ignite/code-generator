80de614d4218931d415fcdb7f307be722a3c528f
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-14 13:09:35 +0630

    chore: use left arrow svg

--- src/components/Instruction.vue
+++ src/components/Instruction.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="preview">
     <h2>Choose a template on the left to get started.</h2>
-    <span class="iconify animate-bounce" data-icon="carbon:arrow-left"></span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      aria-hidden="true"
+      role="img"
+      class="iconify iconify--carbon animate-bounce"
+      width="32"
+      height="32"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 32 32"
+    >
+      <path
+        d="M14 26l1.41-1.41L7.83 17H28v-2H7.83l7.58-7.59L14 6L4 16l10 10z"
+        fill="currentColor"
+      ></path>
+    </svg>
   </div>
 </template>
 
