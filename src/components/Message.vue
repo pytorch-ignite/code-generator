e49882c732b60d932cfdc09b618b26889ed09959
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-15 20:44:44 +0630

    feat: add error/info message box
    Show error if template has not been chosen when click download
    Show error if there is no value for required configs when click download
    Show info after copying code to clipboard.

---
+++ src/components/Message.vue
@@ -0,0 +1,67 @@
+<template>
+  <Transition name="fade">
+    <div class="msg-wrapper" v-if="msg.showMsg" @click="msg.showMsg = false">
+      <div class="msg" :style="style">
+        {{ message }}
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+import { computed, toRefs } from 'vue'
+import { msg } from '../store.js'
+
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const { message, color } = toRefs(props)
+    const style = computed(() => {
+      return {
+        borderColor: color.value,
+        color: color.value,
+        background: color.value + '33'
+      }
+    })
+
+    return { message, color, style, msg }
+  }
+}
+</script>
+
+<style scoped>
+.msg-wrapper {
+  position: fixed;
+  --msg-offset: 1.5rem;
+  bottom: var(--msg-offset);
+  right: var(--msg-offset);
+  left: 0;
+  padding-left: var(--msg-offset);
+  width: inherit;
+}
+.msg {
+  border-radius: 8px;
+  border-width: 2px;
+  border-style: solid;
+  margin-right: var(--msg-offset);
+  padding: 1rem 2rem;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.25s ease-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translate(0, 10px);
+}
+</style>
