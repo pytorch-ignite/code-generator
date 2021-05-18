ee3639e1f46fcbda697c1d9307f5c546ed3fe721
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-05-18 18:36:46 +0630

    chore: add a home page (#124)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 // main entrypoint of the app
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './views/Home.vue'
+import Create from './views/Create.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'create',
+    path: '/create',
+    component: Create
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      }
+    }
+  }
+})
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/Create.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <NavBar />
+    <SplitPane>
+      <template #left>
+        <PaneLeft />
+      </template>
+      <template #right>
+        <PaneRight />
+      </template>
+    </SplitPane>
+    <Footer />
+  </main>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+import SplitPane from '../components/PaneSplit.vue'
+import PaneRight from '../components/PaneRight.vue'
+import PaneLeft from '../components/PaneLeft.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  components: {
+    NavBar,
+    SplitPane,
+    PaneRight,
+    PaneLeft,
+    Footer
+  }
+}
+</script>
