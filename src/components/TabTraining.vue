69f01edcf1c6b1d8ba5bbffe58d335084d5438d3
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-05-20 21:37:46 +0630

    fix: change back to have previous UI (#126)
    - have default value in `output_dir` and `log_every_iters`
    - distributed training checkbox
    - distributed default values (`nproc_per_node`, `nnodes`, `master_addr`, and `master_port`)
    - updated READMEs
    - updated config.yaml

--- src/components/TabTraining.vue
+++ src/components/TabTraining.vue
@@ -7,10 +7,36 @@
       :saveKey="deterministic.name"
     />
     <h2 class="training">Distributed Training (NCCL backend)</h2>
-    <FormRadio :options="[launch, spawn]" saveKey="dist" />
-    <template v-for="(d, index) in distributedConfigs" :key="index">
-      <FormInput :label="d.description" :type="d.type" :saveKey="d.name" />
-    </template>
+    <FormCheckbox label="Use distributed training" saveKey="use_dist" />
+    <div v-show="store.config.use_dist">
+      <FormRadio :options="[launch, spawn]" saveKey="dist" defaultV="launch" />
+      <FormInput
+        :label="nproc_per_node.description"
+        :type="nproc_per_node.type"
+        :saveKey="nproc_per_node.name"
+        :defaultV="nproc_per_node.default"
+      />
+      <FormInput
+        :label="nnodes.description"
+        :type="nnodes.type"
+        :saveKey="nnodes.name"
+        :defaultV="nnodes.default"
+      />
+      <FormInput
+        :label="master_addr.description"
+        :type="master_addr.type"
+        :saveKey="master_addr.name"
+        :defaultV="master_addr.default"
+        v-show="store.config.nnodes > 1"
+      />
+      <FormInput
+        :label="master_port.description"
+        :type="master_port.type"
+        :saveKey="master_port.name"
+        :defaultV="master_port.default"
+        v-show="store.config.nnodes > 1"
+      />
+    </div>
   </div>
 </template>
 
@@ -20,26 +46,37 @@ import { training } from '../metadata/metadata.json'
 import FormCheckbox from './FormCheckbox.vue'
 import FormInput from './FormInput.vue'
 import FormRadio from './FormRadio.vue'
+import { store } from '../store.js'
 
 export default {
   components: { FormCheckbox, FormInput, FormRadio },
   setup() {
-    const { deterministic, launch, spawn, ...distributedConfigs } = training
+    const {
+      deterministic,
+      launch,
+      spawn,
+      nproc_per_node,
+      nnodes,
+      master_addr,
+      master_port
+    } = training
     const isDeterministic = ref(false)
-    const distributedValue = ref({})
 
     // computed properties
     const saveDeterministic = computed(() => {
       saveConfig(deterministic.name, isDeterministic.value)
     })
     return {
+      store,
+      deterministic,
       launch,
       spawn,
-      deterministic,
+      nproc_per_node,
+      nnodes,
+      master_addr,
+      master_port,
       isDeterministic,
       saveDeterministic,
-      distributedConfigs,
-      distributedValue,
       saveDistributed
     }
   }
