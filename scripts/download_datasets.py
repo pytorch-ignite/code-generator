38322e51901c3b52fc2a12453096a3376b6fcad7
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2021-05-24 22:35:37 +0630

    fix: cache datasets and mnist -> cifar10

--- scripts/download_datasets.py
+++ scripts/download_datasets.py
@@ -1,7 +1,7 @@
 from torchvision import datasets
 
-datasets.MNIST("~/data", train=True, download=True)
-datasets.MNIST("~/data", train=False, download=True)
+datasets.CIFAR10("~/data", train=True, download=True)
+datasets.CIFAR10("~/data", train=False, download=True)
 
 datasets.VOCSegmentation(
     "~/data", year="2012", image_set="train", download=True
