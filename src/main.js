ea8e4b790a877bb4ad640fa3a72825d0dff3ef4b
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-06-07 16:13:29 +0630

    fix: scroll to top when navigate (#151)

--- src/main.js
+++ src/main.js
@@ -23,11 +23,9 @@ const router = createRouter({
   routes: routes,
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: 'smooth'
-      }
+      return { el: to.hash, behavior: 'smooth' }
     }
+    return { top: 0, behavior: 'smooth' }
   }
 })
 
