152da3b4d87358d1e147dd1ceac73620eb2e7e9a
Taras Savchyn <trsvchn@gmail.com>
2021-08-10 13:59:04 +0300

    Try resnet50 backbone

--- src/templates/template-vision-segmentation/models.py
+++ src/templates/template-vision-segmentation/models.py
@@ -1,5 +1,5 @@
-from torchvision.models.segmentation import deeplabv3_resnet101
+from torchvision.models.segmentation import deeplabv3_resnet50
 
 
 def setup_model(config):
-    return deeplabv3_resnet101(num_classes=config.num_classes)
+    return deeplabv3_resnet50(num_classes=config.num_classes)
