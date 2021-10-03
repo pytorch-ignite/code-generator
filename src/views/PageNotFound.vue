74d32585c1eb35628a43880309bdf6aae134c790
afzal442 <afzal442@gmail.com>
2021-10-03 10:27:33 +0530

    adds 404 pagenotfound
    Signed-off-by: afzal442 <afzal442@gmail.com>

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="heading">
+    <h1>404 Page Not Found</h1>
+    <p>
+      Oops we couldn't find that page. Try going
+      <RouterLink to="/" class="link alt" :style="learnMore">home</RouterLink>
+    </p>
+  </div>
+</template>
+  <style scoped>
+h1{
+  color: orange;
+}
+.heading {
+  max-width: 75vw;
+  margin: auto;
+  text-align: center;
+}
+.heading .alt {
+  background: var(--c-white);
+  color: var(--c-brand-red);
+}
+</style>
