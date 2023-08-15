8df06097d3929889cfec7dc87639acbd63d2c653
Aryan Gupta <guptaaryan16@gmail.com>
2023-08-15 17:17:55 +0530

    Changed the config copy logic in templates

--- src/templates/template-vision-segmentation/main.py
+++ src/templates/template-vision-segmentation/main.py
@@ -39,17 +39,18 @@ def run(local_rank: int, config: Any):
     # create output folder and copy config file to output dir
     config.output_dir = setup_output_dir(config, rank)
     if rank == 0:
-        #::: if ((it.argparser == 'fire')) { :::#
         with open(f"{config.output_dir}/config-lock.yaml", "a+") as f:
+            #::: if ((it.argparser == 'fire')) { :::#
             for key, value in config.items():
                 if value is not None:
                     f.write(f"{key}: {value}\n")
 
-        #::: } else { :::#
-        copy(config.config, f"{config.output_dir}/config-lock.yaml")
+            #::: } else { :::#
+            for key, value in vars(config).items():
+                if value is not None:
+                    f.write(f"{key}: {value}\n")
 
         #::: } :::#
-
     # donwload datasets and create dataloaders
     dataloader_train, dataloader_eval = setup_data(config)
     le = len(dataloader_train)
