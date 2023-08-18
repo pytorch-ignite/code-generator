733bcfece9fba6e0ebea3908da6015b9d5fae2a2
Aryan Gupta <guptaaryan16@gmail.com>
2023-08-18 23:27:18 +0530

    tests fix

--- src/templates/template-common/test_all.py
+++ src/templates/template-common/test_all.py
@@ -2,9 +2,9 @@ def test_save_config():
     with open("./config.yaml", "r") as f:
         config = yaml.safe_load(f.read())
 
-    save_config(config, "./save-config-test.yaml")
+    save_config(config, "./")
 
-    with open("./", "r") as f:
+    with open("./config-lock.yaml", "r") as f:
         test_config = yaml.safe_load(f.read())
 
     assert config == test_config
