4822e98dc7458673977d32ca6ba935198b140190
Aryan Gupta <guptaaryan16@gmail.com>
2023-08-18 14:19:37 +0530

    fmt fix

--- src/templates/template-vision-segmentation/main.py
+++ src/templates/template-vision-segmentation/main.py
@@ -35,10 +35,10 @@ def run(local_rank: int, config: Any):
 
     # create output folder and copy config file to output dir
     config.output_dir = setup_output_dir(config, rank)
-   
+
     if rank == 0:
         save_config(config)
-        
+
     # donwload datasets and create dataloaders
     dataloader_train, dataloader_eval = setup_data(config)
     le = len(dataloader_train)
