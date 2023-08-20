5849c0e8ab976ffddfb7d6d32eb623e55a134aca
Jyotirmay Khavasi <jyotirmay.khavasi@gmail.com>
2023-08-20 19:25:27 +0530

    Added combined hash of the code for formation of uuid

--- functions/colab.js
+++ functions/colab.js
@@ -2,21 +2,23 @@
 // `external_node_modules` of [functions] in netlify.toml
 // They are required for this function to run
 
-import { v4 as uuidv4 } from 'uuid'
+import { v5 as uuidv5 } from 'uuid';
 import JSZip from 'jszip'
 import { pushToGitHub } from './utils'
 
 const repoOwner = process.env.VUE_APP_GH_USER
 const repo = process.env.VUE_APP_GH_REPO
 
+const NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341'
+
 // This function is the one Netlify function runs on
 // https://docs.netlify.com/functions/build-with-javascript/#synchronous-function-format
 exports.handler = async function (event, _) {
   // event is a JSON object
-  const nbUid = uuidv4()
   const data = JSON.parse(event.body)
   const zip = new JSZip()
   const code = data.code
+  let hash = ''
   const template = `ignite-${data.template}`
   const nbName = `${template}.ipynb`
 
@@ -25,8 +27,10 @@ exports.handler = async function (event, _) {
   // generate a base64 format for pushing to GitHub
   // with Octokit.
   for (const filename in code) {
+    hash += code[filename]
     zip.file(filename, code[filename])
   }
+  const nbUid = uuidv5(hash, NAMESPACE)
   const content = await zip.generateAsync({ type: 'base64' })
   const zipRes = await pushToGitHub(
     content,
