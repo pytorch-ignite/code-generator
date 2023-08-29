5d20e6e97f0f59a32a91f9e730ce66c827dcb10d
Aryan Gupta <guptaaryan16@gmail.com>
2023-08-29 12:45:50 +0530

    fix test_all.py

--- src/templates/template-common/test_all.py
+++ src/templates/template-common/test_all.py
@@ -2,7 +2,11 @@ def test_save_config():
     with open("./config.yaml", "r") as f:
         config = OmegaConf.load(f)
 
+    config.sub_output_dir = "job-dir"
+
     save_config(config, "./")
+
+    del config["sub_output_dir"]
 
     with open("./config-lock.yaml", "r") as f:
         test_config = OmegaConf.load(f)
