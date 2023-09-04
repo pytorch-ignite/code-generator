8dfacced3b69696c835e603c0bb60c173e096009
Aryan Gupta <97878444+guptaaryan16@users.noreply.github.com>
2023-09-04 19:50:34 +0530

    remove extra lines in setup_config for Argparser (#313)

--- src/templates/template-common/utils.py
+++ src/templates/template-common/utils.py
@@ -90,11 +90,8 @@ def setup_config(parser=None):
         parser = get_default_parser()
 
     args = parser.parse_args()
-
     config_path = args.config
-
     config = OmegaConf.load(config_path)
-
     config.backend = args.backend
 
     return config
