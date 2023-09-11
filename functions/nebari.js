73339b84ac3b56601a772daeb56c2b60427f8916
Aryan Gupta <guptaaryan16@gmail.com>
2023-09-11 15:03:21 +0530

    To fix last commit issues in Nebari

--- functions/nebari.js
+++ functions/nebari.js
@@ -7,6 +7,7 @@ import {
 
 const repoOwner = process.env.VUE_APP_GH_USER
 const repo = process.env.VUE_APP_GH_REPO
+const commit = __COMMIT__ /* from vite.config.js */
 
 // This function is the one Netlify function runs on
 // https://docs.netlify.com/functions/build-with-javascript/#synchronous-function-format
@@ -32,7 +33,7 @@ exports.handler = async function (event, _) {
   const nb = getNbCells(title, zipRes, argparser, template, (nebari = true))
 
   // Updating UUID for nebari-test-fix
-  const nbUid_nebari = nbUid + '-nebari'
+  const nbUid_nebari = nbUid + '-nebari' + -commit
 
   // Create the notebook on GitHub
   await pushToGitHub(
