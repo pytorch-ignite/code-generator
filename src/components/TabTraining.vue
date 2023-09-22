7904e5d60208f909be17248fe6c6ed433669202b
Aryan Gupta <guptaaryan16@gmail.com>
2023-09-22 18:20:26 +0530

    feat: Introduce templateOptions.json file to control options for each template individually

--- src/components/TabTraining.vue
+++ src/components/TabTraining.vue
@@ -7,15 +7,13 @@
     using one of the tools provided below:
 
     <h2>Argument Parser</h2>
-
     <ul>
-      <li>
+      <li v-show="argparserOptions.options.includes('argparse')">
         <a href="https://docs.python.org/3/library/argparse.html" id="arg"
-          >Argparse</a
-        >
+          >Argparse</a>
         - is a python built-in tool to handle command-line arguments
       </li>
-      <li>
+      <li v-show="argparserOptions.options.includes('fire')">
         <a
           href="https://github.com/google/python-fire/blob/master/docs/guide.md"
           id="arg"
@@ -24,37 +22,45 @@
         - transforms Python functions into user-friendly command-line tools,
         ideal for DL experimentation.
       </li>
-      <li>
-        <a href="https://hydra.cc" id="arg">Hydra</a>
+      <li v-show="argparserOptions.options.includes('hydra')">
+        <a href="https://hydra.cc" id="arg"
+        >Hydra</a>
         - Simplifying deep learning experiments through flexible configuration
         management
       </li>
     </ul>
     <FormSelect
-      :label="argparser.description"
-      :options="argparser.options"
-      :saveKey="argparser.name"
-      :defaultV="argparser.default"
+      :label="argparserOptions.description"
+      :options="argparserOptions.options"
+      :saveKey="argparserOptions.name"
+      :defaultV="argparserOptions.default"
+      v-show="argparserOptions.options.length > 0"
     />
-    <h2 class="training">Deterministic Training</h2>
-    <FormCheckbox
-      :label="deterministic.description"
-      :saveKey="deterministic.name"
-    />
+    <!-- Deterministic Options -->
+    <div v-show="templateOptions.deterministic">
+      <h2 class="training">Deterministic Training</h2>
+      <FormCheckbox
+        :label="deterministic.description"
+        :saveKey="deterministic.name"
+      />
+    </div>
+  <!-- Distributed Training -->
+  <div v-show="templateOptions.distTraining">
     <h2 class="training">Distributed Training</h2>
     <FormCheckbox label="Use distributed training" saveKey="use_dist" />
-    <div v-show="store.config.use_dist">
+    <div v-show="store.config.use_dist && templateOptions.distTraining">
       <FormSelect
         required
-        :saveKey="backend.name"
-        :label="backend.description"
-        :options="backend.options"
-        :defaultV="backend.default"
+        :saveKey="backendOptions.name"
+        :label="backendOptions.description"
+        :options="backendOptions.options"
+        :defaultV="backendOptions.default"
       />
       <FormRadio
-        :options="[torchrun, spawn]"
+        :options="distOptions"
         saveKey="dist"
-        defaultV="torchrun"
+        :defaultV="distOptions.length > 0? distOptions[0].name : 'spawn'"
+        v-show="distOptions.length > 0"
       />
       <FormInput
         :label="nproc_per_node.description"
@@ -84,16 +90,18 @@
       />
     </div>
   </div>
+  </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { computed, ref, watch, reactive } from 'vue'
 import { training } from '../metadata/metadata.json'
 import FormCheckbox from './FormCheckbox.vue'
 import FormInput from './FormInput.vue'
 import FormRadio from './FormRadio.vue'
 import FormSelect from './FormSelect.vue'
-import { store } from '../store.js'
+import { saveConfig, store } from '../store.js'
+import { templates } from '../templates/template_options.json'
 
 export default {
   components: { FormCheckbox, FormInput, FormRadio, FormSelect },
@@ -110,25 +118,60 @@ export default {
       master_port
     } = training
     const isDeterministic = ref(false)
-
+    
     // computed properties
-    const saveDeterministic = computed(() => {
-      saveConfig(deterministic.name, isDeterministic.value)
+    const templateOptions = computed(()=>{
+      return templates[store.config.template]['training']
     })
+    const argparserOptions = computed(()=> {
+      return findAvailableOptions(argparser, templateOptions)
+    })
+    const backendOptions = computed(()=>{
+      // To check the state of distTraining
+      if (templateOptions.value.distTraining == false){
+        saveConfig("backend", null)
+        return backend
+      }
+      return findAvailableOptions(backend, templateOptions)
+    })
+    const distOptions = computed(()=> {
+      let dist_options = []
+      if (templateOptions.value.distOptions == "all") {
+        return [spawn, torchrun]
+      }
+      for (const option of [spawn, torchrun]){
+        if (templateOptions.value.distOptions.includes(option.name)){
+          dist_options.push(option)
+        }
+      }
+      console.log('Printing dist options')
+      console.log(dist_options)
+      return dist_options
+    })
+
+    watch(store.config, () => {
+      console.log(templateOptions.value)
+      console.log(store.config.template)
+      console.log(argparserOptions.value)
+    })
+
+    const saveDeterministic = computed(() => {
+      saveConfig(deterministic.name, isDeterministic)
+    })
+
     return {
       store,
-      argparser,
+      argparserOptions,
       deterministic,
-      backend,
-      torchrun,
-      spawn,
+      backendOptions,
+      distOptions,
       nproc_per_node,
       nnodes,
       master_addr,
       master_port,
       isDeterministic,
       saveDeterministic,
-      saveDistributed
+      templateOptions
     }
   }
 }
@@ -136,6 +179,23 @@ export default {
 function saveDistributed(key, value) {
   saveConfig(key, value)
 }
+
+function findAvailableOptions(tempKey, templateOptions){
+  const key = tempKey.name
+  if (templateOptions.value[key] == "all" || !templateOptions.value.hasOwnProperty(key)) {
+    return tempKey
+  } else {
+    store.config[key] = templateOptions.value[key][0];
+  }
+  return reactive({
+    "options": templateOptions.value[key],
+    "default": templateOptions.value[key][0],
+    "description": tempKey.description,
+    "name": key,
+    "type": tempKey.type
+  })
+}
+
 </script>
 
 <style scoped>
